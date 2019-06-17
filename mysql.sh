20683066e60097a5cde1f077ff875bfba59c0f06
SQTB <50972220+SQTB@users.noreply.github.com>
2019-06-16 22:33:16 -0500

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 for i in 50 51 52 53 54 55
 do
+scp /linux-soft/03/mysql/mysql-5.7.17.tar root@192.168.4.$i:/root
 ssh root@192.168.4.$i 
-"scp /linux-soft/03/mysql/mysql-5.7.17.tar;
-tar -xf mysql-5.7.17;
+"tar -xf mysql-5.7.17;
 yum -y install  mysql-community-*.rpm;
-syetemctl restart mysqld
-"
+syetemctl restart mysqld"
 done
